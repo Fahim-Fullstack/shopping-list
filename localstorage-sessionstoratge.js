c6b6f72a1b336a07d15343b03fc2b629d6f25501
Fahim <fhasan5@wgu.edu>
2023-09-26 13:50:34 -0500

    Shopping List

--- localstorage-sessionstoratge.js
+++
@@ -1,6 +0,0 @@
-//localStorage Methods
-
-localStorage.setItem('name', 'Brad'); //Set a value with a key
-localStorage.getItem('name'); //get a value using the key
-localStorage.removeItem('name'); // remove item using the key
-localStorage.clear(); //clear all values
